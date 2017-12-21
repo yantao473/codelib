b49d8431a7fb7cdd81d3436be5135f9644147fd0
yanqing4 <yanqing4@staff.sina.com.cn>
2017-12-21 18:03:11 +0800

    增加车站选择项

--- python/ticket.py
+++ python/ticket.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+stattion_version可以在页面https://kyfw.12306.cn/otn/index/init中按F12选中网络(Network)项
+将station_name.js?station_version=xxxxx 将xxxx写入即可
+"""
 
 import argparse
 import json
 import string
 import random
+import re
 import time
 import requests
 
@@ -79,7 +84,7 @@ STYLE = {
 }
 
 
-def UseStyle(string, mode='', fore='', back=''):
+def useStyle(string, mode='', fore='', back=''):
     mode = '%s' % STYLE['mode'][mode] if mode in STYLE['mode'] else ''
     fore = '%s' % STYLE['fore'][fore] if fore in STYLE['fore'] else ''
     back = '%s' % STYLE['back'][back] if back in STYLE['back'] else ''
@@ -91,15 +96,18 @@ def UseStyle(string, mode='', fore='', back=''):
 
 def getargs():
     parser = argparse.ArgumentParser(description='输入订票日期和车次进行查询')
-    parser.add_argument('-d', '--date', type=str, help='date', required=True)
-    parser.add_argument('-t', '--trips',  type=str, help="trips", required=True)
+    parser.add_argument('-d', '--date', type=str, help='出发时间', required=True)
+    parser.add_argument('-t', '--trips',  type=str, help='车次', required=True)
+    parser.add_argument('-s', '--sstation',  type=str, default='北京南', help='出发地')
+    parser.add_argument('-e', '--estation',  type=str, default='武清', help='目的地')
+    parser.add_argument('-v', '--version',  type=str, default='1.9035', help='station_version版本')
     args = parser.parse_args()
     return args
 
 
-def get_data(d, t):
-    base_url = "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=%s" % d
-    params = "&leftTicketDTO.from_station=VNP&leftTicketDTO.to_station=WWP&purpose_codes=ADULT"
+def get_data(sdate, trips, sstation, estation):
+    base_url = "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=%s" % sdate
+    params = "&leftTicketDTO.from_station=%s&leftTicketDTO.to_station=%s&purpose_codes=ADULT" % (sstation, estation)
     url = '%s%s' % (base_url, params)
     headers = {
         'host': 'kyfw.12306.cn',
@@ -107,7 +115,7 @@ def get_data(d, t):
         'referer': 'https://kyfw.12306.cn/otn/leftTicket/init'
     }
 
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers, timeout=3)
     try:
         tmpdict = json.loads(r.text)
         data = tmpdict.get('data')
@@ -115,7 +123,7 @@ def get_data(d, t):
 
         m = ''
         for i in range(len(result)):
-            if t in result[i]:
+            if trips in result[i]:
                 m = result[i]
                 break
         if m:
@@ -134,21 +142,77 @@ def get_data(d, t):
                 rlist.append(random.choice(string.letters + string.digits))
             rstr = ''.join(rlist)
 
-            msg = '商务座: %s 一等座: %s 二等座: %s 无座: %s' % (shangwu.encode(
+            msg = '日期: %s 车次: %s 商务座: %s 一等座: %s 二等座: %s 无座: %s' % (sdate, trips, shangwu.encode(
                 'utf8'), first.encode('utf8'), second.encode('utf8'), no.encode('utf8'))
             if first == n and second == n and no == n:
-                print UseStyle('%s %s' % (msg, rstr), mode='bold', fore='red')
+                print useStyle('%s %s' % (msg, rstr), mode='bold', fore='red')
             else:
-                print UseStyle('%s %s' % (msg, rstr), mode='bold', fore='green')
-
+                print useStyle('%s %s' % (msg, rstr), mode='bold', fore='green')
     except Exception, e:
         print e
 
 
+def get_station_js():
+    url = 'https://kyfw.12306.cn/otn/index/init'
+    headers = {
+        'host': 'kyfw.12306.cn',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+        'referer': 'https://kyfw.12306.cn/otn/leftTicket/init'
+    }
+
+    r = requests.get(url, headers=headers, timeout=3)
+    pattern = re.compile(r'/otn/resources/js/framework/station_name\.js\?station_version=\d+?\.\d+', re.M | re.I)
+    m = pattern.search(r.text)
+    if m:
+        return m.group()
+    else:
+        return None
+
+
+def get_stations(url):
+    headers = {
+        'host': 'kyfw.12306.cn',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+        'referer': 'https://kyfw.12306.cn/otn/leftTicket/init'
+    }
+
+    r = requests.get(url, headers=headers, timeout=3)
+    info = r.text.replace('var', '') .replace('station_names', '') .replace(
+        '=', '') .replace("'", '') .replace(";", '') .strip()
+    return info.split('|')
+
+
+def get_station_map(station_list, station):
+    if isinstance(station, str):
+        station = station.decode('utf-8')
+    try:
+        mindex = station_list.index(station)
+        return station_list[mindex + 1]
+    except Exception:
+        return None
+
+
 def main():
+    print useStyle('注意: 使用时请传入正确的出发地和目的地编码，默认为出发地为北京南，目的地为武清', mode='bold', fore='yellow')
     args = getargs()
+    c = 0
+    try_times = 3
+    while c < try_times:
+        js = get_station_js()
+        if js:
+            stations_url = 'https://kyfw.12306.cn%s' % js
+            break
+        c = c + 1
+    else:
+        v = raw_input('请输入station_name.js的版本:')
+        stations_url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name\.js\?station_version=%s' % v
+
+    stations = get_stations(stations_url)
+    sstation = get_station_map(stations, args.sstation)
+    estation = get_station_map(stations, args.estation)
+
     while True:
-        get_data(args.date, args.trips)
+        get_data(args.date, args.trips, sstation, estation)
         time.sleep(3)
 
 
