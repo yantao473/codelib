96f94347c1ed0b7871fc8327271ecadf579718e1
yanqing4 <yanqing4@staff.sina.com.cn>
2018-04-13 12:16:25 +0800

    添加车站编码标识

--- python/ticket.py
+++ python/ticket.py
@@ -1,32 +1,60 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import argparse
+import datetime
 import json
+import os
+import re
 import time
-from datetime import datetime
 
 import requests
 
 import prettytable as pt
 
 
+def get_stations_code():
+    domain = 'https://kyfw.12306.cn'
+    action = '/otn/index/init'
+    headers = {
+        'host': 'kyfw.12306.cn',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84',
+        'referer': 'https://kyfw.12306.cn/otn/leftTicket/init'
+    }
+    url = '%s%s' % (domain, action)
+    r = requests.get(url, headers=headers, timeout=3)
+
+    m = re.search(r'station_version=(\d+?\.\d+)', r.text)
+    if m:
+        mapdict = {}
+        saction = '/otn/resources/js/framework/station_name.js?%s' % m.group()
+        surl = '%s%s' % (domain, saction)
+        sr = requests.get(surl, headers=headers, timeout=3)
+        c = sr.text.replace('var', '').replace('station_names', '').replace(
+            '=', '').replace("'", '').replace(';', '').strip().lstrip('@')
+        slist = c.split('@')
+        for s in slist:
+            t = s.split('|')
+            mapdict[t[1]] = t[2]
+
+        if mapdict:
+            with open('station_code.txt', 'w') as f:
+                f.write(json.dumps(mapdict))
+
+
 def getargs():
-    curdate = datetime.now().strftime('%Y-%m-%d')
+    curtime = datetime.datetime.now().strftime('%Y-%m-%d')
     parser = argparse.ArgumentParser(description='输入订票日期和车次进行查询')
-    parser.add_argument('-d', '--train_date', type=str, default=curdate, help='出发时间')
+    parser.add_argument('-d', '--train_date', type=str,  default=curtime, help='出发时间')
     parser.add_argument('-t', '--train_number', type=str, default='C2239', help='车次')
-    parser.add_argument('-s', '--start_station', type=str, default='VNP', help='出发地 北京南为VNP')
-    parser.add_argument('-e', '--end_station', type=str, default='WWP', help='目的地 武清为WWP')
+    parser.add_argument('-s', '--start_station', type=str, default='北京南', help='出发地')
+    parser.add_argument('-e', '--end_station', type=str, default='武清', help='目的地')
+    parser.add_argument('-u', '--update_stations', type=bool, default=False, help='更新站点编码列表')
     args = parser.parse_args()
     return args
 
 
-def get_data(args):
-    sdate = args.train_date
-    trainNumber = args.train_number
-    sstation = args.start_station
-    estation = args.end_station
-
+def get_data(sdate, trainNumber, sstation, estation):
     domain = 'https://kyfw.12306.cn/otn/leftTicket/query'
     tdate = 'leftTicketDTO.train_date=%s' % sdate
     cfrom = 'leftTicketDTO.from_station=%s' % sstation
@@ -58,32 +86,34 @@ def get_data(args):
             if not shangwu:
                 shangwu = t[32]
 
-            green = '\033[32;49;1m%s\033[0m'
+            # green = '\033[32;49;1m%s\033[0m'
+            yellow = '\033[33;49;1m%s\033[0m'
             red = '\033[31;49;1m%s\033[0m'
 
             if t[26] == u'无':
                 t[26] = red % t[26]
             else:
-                t[26] = green % t[26]
+                t[26] = yellow % t[26]
 
             if t[30] == u'无':
                 t[30] = red % t[30]
             else:
-                t[30] = green % t[30]
+                t[30] = yellow % t[30]
 
             if t[31] == u'无':
                 t[31] = red % t[31]
             else:
-                t[31] = green % t[31]
+                t[31] = yellow % t[31]
 
-            tb.add_column(u'    车次', [t[3]])
-            tb.add_column(u'乘车日期', [sdate])
+            tb.add_column(u'车次', [t[3]])
+            tb.add_column(u'日期', [sdate[5:]])
             tb.add_column(u'发车时间', [t[8]])
-            tb.add_column(u'到站时间', [t[9]])
-            tb.add_column(u'    无座', [t[26]])
-            tb.add_column(u'  二等座', [t[30]])
-            tb.add_column(u'  一等座', [t[31]])
-            tb.add_column(u'  商务座', [shangwu])
+            # tb.add_column(u'到站时间', [t[9]])
+            tb.add_column(u'无座', [t[26]])
+            tb.add_column(u'二等座', [t[30]])
+            tb.add_column(u'一等座', [t[31]])
+            tb.add_column(u'商务座', [shangwu])
+            tb.add_column(u'标识', [str(int(time.time()))[-2:]])
 
             # for i in range(len(t)):
             #     print '%d --- %s' % (i, t[i])
@@ -91,14 +121,31 @@ def get_data(args):
             print(tb)
 
     except Exception as e:
-        print(e)
+        # print(e)
+        time.sleep(10)
 
 
 def main():
-    print('北京南查询编码为: VNP，武清查询编码为: WWP')
     args = getargs()
+    sfile_name = 'station_code.txt'
+    if not os.path.exists(sfile_name) or not os.path.getsize(sfile_name) or args.update_stations:
+        get_stations_code()
+
+    station_mapdict = {}
+    with open(sfile_name) as f:
+        station_mapdict = json.loads(f.read())
+
+    sstation = args.start_station.decode('utf-8')
+    estation = args.end_station.decode('utf-8')
+
+    sdate = args.train_date
+    trainNumber = args.train_number
+
+    sstation_code = station_mapdict.get(sstation)
+    estation_code = station_mapdict.get(estation)
+
     while True:
-        get_data(args)
+        get_data(sdate, trainNumber, sstation_code, estation_code)
         time.sleep(3)
 
 
