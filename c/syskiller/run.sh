311ee7ab3f5cf85bddc62bde78c23f90c9fdb099
yanqing4 <yanqing4@staff.sina.com.cn>
2019-01-30 15:03:49 +0800

    将cpu占用调整到65%左右

--- c/syskiller/run.sh
+++ c/syskiller/run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 killall syskiller
-killall cpukiller 
 cpu_count=$(cat /proc/cpuinfo  | grep  process  | wc -l )
 ./syskiller $cpu_count
--- c/syskiller/syskiller.c
+++ c/syskiller/syskiller.c
@@ -1,25 +1,17 @@
-#include <sys/sysinfo.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <limits.h>
 #include <sys/types.h>
-#include <time.h>
+#include <sys/time.h>
 
-void kill_cpu();
 
 int main(int argc, char **argv)
 {
-    struct sysinfo info;
-    long d =  (1 << SI_LOAD_SHIFT );
-    float real_load;
-    int fork_nums = 4;
-    int sleep_times = 60;
-
     pid_t pid;
+    int fork_nums = 4;
+    struct timeval tv;
+    long long start_time, end_time;
 
-    float max_float = fork_nums * 0.8;
-    float min_float = fork_nums * 0.6;
 
     if(argc >= 2) {
         fork_nums = atoi(argv[1]);
@@ -30,39 +22,19 @@ int main(int argc, char **argv)
             continue;
         }
 
-        for(;;) {
-            sysinfo(&info);
-            real_load = (float) info.loads[0] / (float) d;
+        while(1) {
+            gettimeofday(&tv, NULL);
+            start_time = tv.tv_sec * 1000000 + tv.tv_usec;
+            end_time = start_time;
 
-            if(real_load <= min_float) {
-                kill_cpu();
+            while((end_time - start_time) < 60000) {
+                gettimeofday(&tv, NULL);
+                end_time = tv.tv_sec * 1000000 + tv.tv_usec;
             }
-
-            if(real_load > max_float) {
-                srand((int)time(NULL));
-                sleep_times = 40 + rand() % 40;
-                sleep(sleep_times);
-            }
+            /* usleep(60000); */
+            usleep(35000); // laod about 0.65 * cpu_counts
         }
     }
 
     return 0;
-}
-
-void kill_cpu()
-{
-    int i, j, k;
-    int m = 1;
-
-    for(i = 1; i < 1000; i++) {
-        for(j = 1; j < 1000; j++) {
-            for(k = 1; k < 1000; k++) {
-                m *= i;
-
-                if(m <= INT_MAX) {
-                    m = 1;
-                }
-            }
-        }
-    }
 }
