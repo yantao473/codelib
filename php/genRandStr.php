a5a06532475cf04b421dab70d22579cfd3961de4
yanqing4 <yanqing4@staff.sina.com.cn>
2020-07-28 10:23:19 +0800

    add strign

---
+++ php/genRandStr.php
@@ -0,0 +1,16 @@
+<?php
+function genRandStr($length)
+{
+    if (function_exists('openssl_random_pseudo_bytes')) {
+        $bytes = openssl_random_pseudo_bytes($length * 2);
+        if ($bytes === false) {
+            throw new RuntimeException('Unable to generate a random string');
+        }
+
+        return substr(str_replace(array('/', '+', '='), '', base64_encode($bytes)), 0, $length);
+    }
+
+    $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    return substr(str_shuffle(str_repeat($pool, 5)), 0, $length);
+}
